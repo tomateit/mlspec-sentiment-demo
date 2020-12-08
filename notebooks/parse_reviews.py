cac69c18df0ffcc19d5ce77e865e01bf1f2f8fd4
Max Ermakov <maksim_ermakov@protonmail.com>
2020-12-08 23:53:26 +0000

    new model WIP

---
+++ notebooks/parse_reviews.py
@@ -0,0 +1,115 @@
+import requests
+import json
+import sqlite3
+import time
+import random
+import re
+
+regexp_catalog_id = re.compile(r"(?:_)\d{5,20}(?:-catalog)")
+regexp_digits = re.compile(r"(?:\D*)(\d{5,20})(?:\D*)")
+
+class Parser():
+    def __init__(self, database_name=None):
+        if not database_name:
+            print("No database name provided. Creating...")
+            database_name = f"reviews_{random.randint(100,999)}.db"
+        self.conn = sqlite3.connect(database_name)
+        print(f"Connected to database {database_name}")
+
+    def _create_tables(self):
+        c = self.conn.cursor()
+        c.execute('''CREATE TABLE catalog_ids (catalog_id text)''')
+        c.execute('''CREATE TABLE reviews (content text, label text)''')
+        self.conn.commit()
+        print("Tables created")
+
+    def _push_id_to_db(self, cid):
+        c = self.conn.cursor()
+        c.execute(f"INSERT INTO catalog_ids VALUES ('{str(cid)}')")
+        self.conn.commit()
+
+    def _push_review_to_db(self, text, label):
+        c = self.conn.cursor()
+        c.execute(f"INSERT INTO reviews VALUES ('{text}','{label}')")
+        self.conn.commit()
+
+    def _get_all_catalog_ids(self):
+        c = self.conn.cursor()
+        catalog_ids = list(c.execute('''SELECT DISTINCT catalog_id FROM catalog_ids'''))
+        catalog_ids = list(map(lambda x: x[0], catalog_ids))
+        return catalog_ids
+
+    def _parse_catalog_ids(self):
+        for n_page in range(1,100):
+            print(f"Doing {n_page} catalog page")
+            response_page = requests.get(f"https://hi-tech.mail.ru/ajax/mobile-catalog/?sort=popular&sort_order=desc&page={n_page}")
+            if (response_page.status_code != 200):
+                print(f"status is not 200 for {response_page.url}: {response_page.status_code}")
+                break
+            cids = regexp_catalog_id.findall(response_page.text)
+            for cid in cids:
+                cid_ = regexp_digits.match(cid).group(1)
+                self._push_id_to_db(cid_)
+            time.sleep(random.randint(1,5))
+
+
+    def _parse_reviews(self, cids=None):
+        if not cids:
+            cids = self._get_all_catalog_ids()
+
+        for cid in cids:
+            try_pages = True
+            page_increment = 1
+            while (try_pages):
+                page = requests.get(f"https://hi-tech.mail.ru/ajax/{cid}-catalog/feedback/list/?page={page_increment}")
+                page_increment += 1
+                try_pages = self._process_page(page)
+                time.sleep(random.randint(1,5))
+    
+    def _process_page(self, response_page):
+        try:
+            # check status
+            if (response_page.status_code != 200):
+                print(f"status is not 200 for {response_page.url}: {response_page.status_code}")
+                return False
+            # check content
+            page_data = json.loads(response_page.text).get("data", [])
+            if (not page_data):
+                print("page does not contain data")
+                return False
+            # try extraction
+            self._extract_reviews_from_data(page_data)
+            print("extracted from ", response_page.url)
+            # extraction was successfull, maybe try next page
+            return True
+        except:
+            print("exception occured")
+            return False
+    
+    def _extract_reviews_from_data(self,data):
+        for rev in data:
+            text_ = {}
+            for t in rev['text']:
+                text_[t["title"]] = t["text"]
+            
+            label = "pos" if rev["vote_type"] == "positive" else "neg"
+            # я буду отдавать позитивной части отзыва если он положительный и негативной - если отрицательный
+            # if (label=="pos"):
+            #     text = f"{text_['Плюсы']} {text_['Впечатления']}"
+            # else:
+            #     text = f"{text_['Минусы']} {text_['Впечатления']}"
+            text = f"{text_['Плюсы']}. {text_['Минусы']}. {text_['Впечатления']}."
+    #         print(label, text)
+            self._push_review_to_db(text, label)
+
+    def main(self):
+        self._create_tables()
+        self._parse_catalog_ids()
+        self._parse_reviews()
+        print("Parsing finished.")
+
+
+
+if __name__ == "__main__":
+    _p = Parser()
+    _p.main()
--- sentiment_classifier.py
+++ sentiment_classifier.py
@@ -1,23 +1,21 @@
-__author__ = 'xead'
-# from sklearn.externals import joblib
 import pickle
 
-
-class SentimentClassifier(object):
+class SentimentClassifierEN(object):
     def __init__(self):
-        with open("./SklearnModel.pkl", "rb") as mfile:
+        with open("./SentimentModelEN.pkl", "rb") as mfile:
             self.model = pickle.load(mfile)
             # self.vectorizer = pickle.load("./BigramUnprocessedVectorizer.pkl")
         self.classes_dict = {0: "negative", 1: "positive", -1: "prediction error"}
 
     @staticmethod
     def get_probability_words(probability):
+        _prob = round(probability, 2)
         if probability < 0.55:
-            return "neutral or uncertain"
+            return f"neutral or uncertain ({_prob})"
         if probability < 0.7:
-            return "probably"
+            return f"probably ({_prob})"
         if probability > 0.95:
-            return "certain"
+            return f"certain ({_prob})"
         else:
             return ""
 
@@ -30,17 +28,44 @@ class SentimentClassifier(object):
             print("prediction error")
             return -1, 0.8
 
-    # def predict_list(self, list_of_texts):
-    #     try:
-    #         vectorized = self.vectorizer.transform(list_of_texts)
-    #         return self.model.predict(vectorized),\
-    #                self.model.predict_proba(vectorized)
-    #     except:
-    #         print("prediction error")
-    #         return None
 
     def get_prediction_message(self, text):
         prediction = self.predict_text(text)
         class_prediction = prediction[0]
         prediction_probability = prediction[1]
         return self.get_probability_words(prediction_probability) + " " + self.classes_dict[class_prediction]
+
+class SentimentClassifierRU(object):
+    def __init__(self):
+        with open("./SentimentModelRU.pkl", "rb") as mfile:
+            self.model = pickle.load(mfile)
+        self.classes_dict = {0: "негативный", 1: "позитивный", -1: "ошибка"}
+
+    @staticmethod
+    def get_probability_words(probability):
+        _prob = round(probability, 2)
+        if probability < 0.55:
+            return f"предположительно ({_prob})"
+        if probability < 0.7:
+            return f"вероятно ({_prob})"
+        if probability > 0.95:
+            return f"определённо ({_prob})"
+        else:
+            return ""
+
+    def predict_text(self, text):
+        try:
+            sent_class = self.model.predict([text])[0]
+            probability = self.model.predict_proba([text])[0].max()
+            return (sent_class, probability)
+        except:
+            print("prediction error")
+            return -1, 0.8
+
+    def get_prediction_message(self, text):
+        prediction = self.predict_text(text)
+        class_prediction = prediction[0]
+        prediction_probability = prediction[1]
+        return self.get_probability_words(prediction_probability) + " " + self.classes_dict[class_prediction]
+
+        
