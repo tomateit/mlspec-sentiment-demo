1fb75e5dac04761be06125e8d6fd47660181f2df
Max Ermakov <maksim_ermakov@protonmail.com>
2020-12-26 07:06:03 +0000

    Added Russian Language sent.classifier

--- sentiment_classifier.py
+++ sentiment_classifier.py
@@ -1,4 +1,5 @@
 import pickle
+import dill
 
 class SentimentClassifierEN(object):
     def __init__(self):
@@ -38,34 +39,36 @@ class SentimentClassifierEN(object):
 class SentimentClassifierRU(object):
     def __init__(self):
         with open("./SentimentModelRU.pkl", "rb") as mfile:
-            self.model = pickle.load(mfile)
+            self.model = dill.load(mfile)
         self.classes_dict = {0: "негативный", 1: "позитивный", -1: "ошибка"}
 
-    @staticmethod
-    def get_probability_words(probability):
-        _prob = round(probability, 2)
-        if probability < 0.55:
-            return f"предположительно ({_prob})"
-        if probability < 0.7:
-            return f"вероятно ({_prob})"
-        if probability > 0.95:
-            return f"определённо ({_prob})"
-        else:
-            return ""
+    # @staticmethod
+    #def get_probability_words(probability):
+        #_prob = round(probability, 2)
+        #if probability < 0.55:
+            #return f"предположительно ({_prob})"
+        #if probability < 0.7:
+            #return f"вероятно ({_prob})"
+        #if probability > 0.95:
+            #return f"определённо ({_prob})"
+        #else:
+            #return ""
 
     def predict_text(self, text):
         try:
             sent_class = self.model.predict([text])[0]
-            probability = self.model.predict_proba([text])[0].max()
-            return (sent_class, probability)
+            #probability = self.model.predict_proba([text])[0].max()
+            return sent_class
+            #return (sent_class, probability)
         except:
             print("prediction error")
-            return -1, 0.8
+            #return -1, 0.8
+            return -1
 
     def get_prediction_message(self, text):
         prediction = self.predict_text(text)
-        class_prediction = prediction[0]
-        prediction_probability = prediction[1]
-        return self.get_probability_words(prediction_probability) + " " + self.classes_dict[class_prediction]
+        #class_prediction = prediction[0]
+        #prediction_probability = prediction[1]
+        return f"  {self.classes_dict[prediction]}"
 
         
